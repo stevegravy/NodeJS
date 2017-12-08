b23b646c1fb72cc3b40809fe2cc159e2768c3d37
Unknown <contact@stevegravy.com>
2017-12-08 16:44:20 +0100

    exo chat maybe ok

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs');
 
-app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
-http.listen(8080, function(){
-    console.log('Bonjour, moi, c\'est Steve. Puis-je vous aider?');
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
 });
+
+server.listen(8080);
+
